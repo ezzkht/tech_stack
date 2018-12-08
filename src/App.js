62843724fdacb0db617a02048c746fa57cf577b8
Ezz Khayyat <ekhayyat@gmail.com>
2018-12-08 23:03:07 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Header} from './components/common';
+import LibraryList from './components/LibraryList';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <View style={{flex:1}}>
+                    <Header headerText="Tech Stack"/>
+                    <LibraryList/>
+                </View>
+          </Provider>
+        );
+    }
+}
